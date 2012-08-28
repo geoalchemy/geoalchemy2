40c7f50c56333b97878bd1e77acdc955905e445b
Éric Lemoine <eric.lemoine@gmail.com>
2012-08-28 23:33:22 +0200

    minor test refactoring, and additional tests

--- geoalchemy2/tests/test_types.py
+++ geoalchemy2/tests/test_types.py
@@ -9,23 +9,24 @@ def eq_sql(a, b, msg=None):
     eq_(a, b, msg)
 
 
-class TestGeometry(unittest.TestCase):
+def _create_table():
+    from sqlalchemy import Table, MetaData, Column
+    from geoalchemy2.types import Geometry
+    table = Table('table', MetaData(), Column('geom', Geometry))
+    return table
 
-    def _create_table(self):
-        from sqlalchemy import Table, MetaData, Column
-        from geoalchemy2.types import Geometry
-        table = Table('table', MetaData(), Column('geom', Geometry))
-        return table
+
+class TestGeometry(unittest.TestCase):
 
     def test_column_expression(self):
         from sqlalchemy.sql import select
-        table = self._create_table()
+        table = _create_table()
         s = select([table.c.geom])
         eq_sql(s, 'SELECT ST_AsBinary("table".geom) AS geom_1 FROM "table"')
 
     def test_select_bind_expression(self):
         from sqlalchemy.sql import select
-        table = self._create_table()
+        table = _create_table()
         s = select(['foo']).where(table.c.geom == 'POINT(1 2)')
         eq_sql(s, 'SELECT foo FROM "table" WHERE '
                   '"table".geom = ST_GeomFromText(:geom_1)')
@@ -33,14 +34,14 @@ class TestGeometry(unittest.TestCase):
 
     def test_insert_bind_expression(self):
         from sqlalchemy.sql import insert
-        table = self._create_table()
+        table = _create_table()
         i = insert(table).values(geom='POINT(1 2)')
         eq_sql(i, 'INSERT INTO "table" (geom) VALUES (ST_GeomFromText(:geom))')
         eq_(i.compile().params, {'geom': 'POINT(1 2)'})
 
     def test_function_call(self):
         from sqlalchemy.sql import select
-        table = self._create_table()
+        table = _create_table()
         s = select([table.c.geom.ST_Buffer(2)])
         eq_sql(s,
                'SELECT ST_AsBinary(ST_Buffer("table".geom, :param_1)) '
@@ -48,47 +49,79 @@ class TestGeometry(unittest.TestCase):
 
     @raises(AttributeError)
     def test_non_ST_function_call(self):
-        table = self._create_table()
+        table = _create_table()
         table.c.geom.Buffer(2)
+
+
+class TestFunction(unittest.TestCase):
+
+    def test_ST_Equal_WKTElement_WKTElement(self):
+        from sqlalchemy import func
+        from geoalchemy2.types import WKTElement
+        expr = func.ST_Equals(WKTElement('POINT(1 2)'),
+                              WKTElement('POINT(1 2)'))
+        eq_sql(expr,
+               'ST_Equals(ST_GeomFromText(:ST_GeomFromText_1), '
+               'ST_GeomFromText(:ST_GeomFromText_2))')
+        eq_(expr.compile().params,
+            {u'ST_GeomFromText_1': 'POINT(1 2)',
+             u'ST_GeomFromText_2': 'POINT(1 2)'})
+
+    def test_ST_Equal_Column_WKTElement(self):
+        from sqlalchemy import func
+        from geoalchemy2.types import WKTElement
+        table = _create_table()
+        expr = func.ST_Equals(table.c.geom, WKTElement('POINT(1 2)'))
+        eq_sql(expr,
+               'ST_Equals("table".geom, '
+               'ST_GeomFromText(:ST_GeomFromText_1))')
+        eq_(expr.compile().params, {u'ST_GeomFromText_1': 'POINT(1 2)'})
 
 
 class TestOperator(unittest.TestCase):
 
-    def _create_table(self):
-        from sqlalchemy import Table, MetaData, Column
-        from geoalchemy2.types import Geometry
-        table = Table('table', MetaData(), Column('geom', Geometry))
-        return table
-
     def test_eq(self):
-        table = self._create_table()
+        table = _create_table()
         expr = table.c.geom == 'POINT(1 2)'
         eq_sql(expr, '"table".geom = ST_GeomFromText(:geom_1)')
 
     def test_eq_with_None(self):
-        table = self._create_table()
+        table = _create_table()
         expr = table.c.geom == None
         eq_sql(expr, '"table".geom IS NULL')
 
     def test_ne(self):
-        table = self._create_table()
+        table = _create_table()
         expr = table.c.geom != 'POINT(1 2)'
         eq_sql(expr, '"table".geom != ST_GeomFromText(:geom_1)')
 
     def test_ne_with_None(self):
-        table = self._create_table()
+        table = _create_table()
         expr = table.c.geom != None
         eq_sql(expr, '"table".geom IS NOT NULL')
 
     def test_intersects(self):
-        table = self._create_table()
+        table = _create_table()
         expr = table.c.geom.intersects('POINT(1 2')
         eq_sql(expr, '"table".geom && ST_GeomFromText(:geom_1)')
 
     def test_overlaps_or_left(self):
-        table = self._create_table()
+        table = _create_table()
         expr = table.c.geom.overlaps_or_left('POINT(1 2')
         eq_sql(expr, '"table".geom &< ST_GeomFromText(:geom_1)')
+
+    def test_overlaps_or_right(self):
+        table = _create_table()
+        expr = table.c.geom.overlaps_or_right('POINT(1 2')
+        eq_sql(expr, '"table".geom &> ST_GeomFromText(:geom_1)')
+
+
+class TestWKTElement(unittest.TestCase):
+
+    def test_desc(self):
+        from geoalchemy2.types import WKTElement
+        e = WKTElement('POINT(1 2)')
+        eq_(e.desc, 'POINT(1 2)')
 
 
 class TestWKBElement(unittest.TestCase):
