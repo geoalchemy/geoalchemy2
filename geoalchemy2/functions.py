9f3a3c5bfa08717ac6d474b18d0159ed8bd1df95
Éric Lemoine <eric.lemoine@gmail.com>
2012-09-04 15:33:33 +0200

    add functions

--- geoalchemy2/functions.py
+++ geoalchemy2/functions.py
@@ -55,8 +55,28 @@ class ST_Envelope(GenericFunction):
     type = types.Geometry
 
 
+class ST_GeometryN(GenericFunction):
+    name = 'ST_GeometryN'
+
+
 class ST_GeometryType(GenericFunction):
     name = 'ST_GeometryType'
+
+
+class ST_IsValid(GenericFunction):
+    name = 'ST_IsValid'
+
+
+class ST_NPoints(GenericFunction):
+    name = 'ST_NPoints'
+
+
+class ST_X(GenericFunction):
+    name = 'ST_X'
+
+
+class ST_Y(GenericFunction):
+    name = 'ST_Y'
 
 
 #
@@ -64,8 +84,33 @@ class ST_GeometryType(GenericFunction):
 #
 
 
+class ST_AsBinary(GenericFunction):
+    name = 'ST_AsBinary'
+
+
+class ST_AsGeoJSON(GenericFunction):
+    name = 'ST_AsGeoJSON'
+
+
+class ST_AsGML(GenericFunction):
+    name = 'ST_AsGML'
+
+
+class ST_AsKML(GenericFunction):
+    name = 'ST_AsKML'
+
+
+class ST_AsSVG(GenericFunction):
+    name = 'ST_AsSVG'
+
+
 class ST_AsText(GenericFunction):
     name = 'ST_AsText'
+
+
+class ST_AsText(GenericFunction):
+    name = 'ST_AsText'
+
 
 #
 # Spatial Relationships and Measurements
@@ -76,12 +121,88 @@ class ST_Area(GenericFunction):
     name = 'ST_Area'
 
 
+class ST_Centroid(GenericFunction):
+    """
+    Return type: :class:`geoalchemy2.types.Geometry`.
+    """
+    name = 'ST_Centroid'
+    type = types.Geometry
+
+
 class ST_Contains(GenericFunction):
     name = 'ST_Contains'
 
 
+class ST_ContainsProperly(GenericFunction):
+    name = 'ST_ContainsProperly'
+
+
+class ST_Covers(GenericFunction):
+    name = 'ST_Covers'
+
+
+class ST_CoveredBy(GenericFunction):
+    name = 'ST_CoveredBy'
+
+
+class ST_Crosses(GenericFunction):
+    name = 'ST_Crosses'
+
+
+class ST_Disjoint(GenericFunction):
+    name = 'ST_Disjoint'
+
+
+class ST_Distance(GenericFunction):
+    name = 'ST_Distance'
+
+
+class ST_DistanceSphere(GenericFunction):
+    name = 'ST_DistanceSphere'
+
+
+class ST_DFullyWithin(GenericFunction):
+    name = 'ST_DFullyWithin'
+
+
+class ST_DWithin(GenericFunction):
+    name = 'ST_DWithin'
+
+
+class ST_Equals(GenericFunction):
+    name = 'ST_Equals'
+
+
 class ST_Intersects(GenericFunction):
     name = 'ST_Intersects'
+
+
+class ST_Length(GenericFunction):
+    name = 'ST_Length'
+
+
+class ST_OrderingEquals(GenericFunction):
+    name = 'ST_OrderingEquals'
+
+
+class ST_Overlaps(GenericFunction):
+    name = 'ST_Overlaps'
+
+
+class ST_Perimeter(GenericFunction):
+    name = 'ST_Perimeter'
+
+
+class ST_Relate(GenericFunction):
+    name = 'ST_Relate'
+
+
+class ST_Touches(GenericFunction):
+    name = 'ST_Touches'
+
+
+class ST_Within(GenericFunction):
+    name = 'ST_Within'
 
 
 #
@@ -94,4 +215,28 @@ class ST_Buffer(GenericFunction):
     Return type: :class:`geoalchemy2.types.Geometry`.
     """
     name = 'ST_Buffer'
+    type = types.Geometry
+
+
+class ST_Difference(GenericFunction):
+    """
+    Return type: :class:`geoalchemy2.types.Geometry`.
+    """
+    name = 'ST_Difference'
+    type = types.Geometry
+
+
+class ST_Intersection(GenericFunction):
+    """
+    Return type: :class:`geoalchemy2.types.Geometry`.
+    """
+    name = 'ST_Intersection'
+    type = types.Geometry
+
+
+class ST_Union(GenericFunction):
+    """
+    Return type: :class:`geoalchemy2.types.Geometry`.
+    """
+    name = 'ST_Union'
     type = types.Geometry
--- geoalchemy2/tests/test_functions.py
+++ geoalchemy2/tests/test_functions.py
@@ -1,18 +1,181 @@
+import re
+
 from nose.tools import eq_
+
+from sqlalchemy.sql import func
 
 #
 # Importing geoalchemy2 actually registers the GeoAlchemy generic
 # functions in SQLAlchemy's function registry.
 #
 
-import geoalchemy2  # NOQA
+import geoalchemy2.functions  # NOQA
 
 
-def test_GeometryType():
-    from sqlalchemy.sql import func
-    eq_(str(func.ST_GeometryType(1)), 'ST_GeometryType(:param_1)')
+def eq_sql(a, b, msg=None):
+    a = re.sub(r'[\n\t]', '', str(a))
+    eq_(a, b, msg)
 
 
-def test_Buffer():
-    from sqlalchemy.sql import func
-    eq_(str(func.ST_Buffer(1)), 'ST_Buffer(:param_1)')
+def _test_simple_func(name):
+    eq_sql(getattr(func, name)(1).select(),
+           'SELECT %(name)s(:param_1) AS "%(name)s_1"' % \
+                   dict(name=name))
+
+
+def _test_geometry_returning_func(name):
+    eq_sql(getattr(func, name)(1).select(),
+           'SELECT ST_AsBinary(%(name)s(:param_1)) AS "%(name)s_1"' % \
+                   dict(name=name))
+
+
+def test_ST_Envelope():
+    _test_geometry_returning_func('ST_Envelope')
+
+
+def test_ST_GeometryN():
+    _test_simple_func('ST_GeometryN')
+
+
+def test_ST_GeometryType():
+    _test_simple_func('ST_GeometryType')
+
+
+def test_ST_IsValid():
+    _test_simple_func('ST_IsValid')
+
+
+def test_ST_NPoints():
+    _test_simple_func('ST_NPoints')
+
+
+def test_ST_X():
+    _test_simple_func('ST_X')
+
+
+def test_ST_Y():
+    _test_simple_func('ST_Y')
+
+
+def test_ST_AsBinary():
+    _test_simple_func('ST_AsBinary')
+
+
+def test_ST_AsGeoJSON():
+    _test_simple_func('ST_AsGeoJSON')
+
+
+def test_ST_AsGML():
+    _test_simple_func('ST_AsGML')
+
+
+def test_ST_AsKML():
+    _test_simple_func('ST_AsKML')
+
+
+def test_ST_AsSVG():
+    _test_simple_func('ST_AsSVG')
+
+
+def test_ST_AsText():
+    _test_simple_func('ST_AsText')
+
+
+def test_ST_Area():
+    _test_simple_func('ST_Area')
+
+
+def test_ST_Centroid():
+    _test_geometry_returning_func('ST_Centroid')
+
+
+def test_ST_Contains():
+    _test_simple_func('ST_Contains')
+
+
+def test_ST_ContainsProperly():
+    _test_simple_func('ST_ContainsProperly')
+
+
+def test_ST_Covers():
+    _test_simple_func('ST_Covers')
+
+
+def test_ST_CoveredBy():
+    _test_simple_func('ST_CoveredBy')
+
+
+def test_ST_Crosses():
+    _test_simple_func('ST_Crosses')
+
+
+def test_ST_Disjoint():
+    _test_simple_func('ST_Disjoint')
+
+
+def test_ST_Distance():
+    _test_simple_func('ST_Distance')
+
+
+def test_ST_DistanceSphere():
+    _test_simple_func('ST_DistanceSphere')
+
+
+def test_ST_DFullyWithin():
+    _test_simple_func('ST_DFullyWithin')
+
+
+def test_ST_DWithin():
+    _test_simple_func('ST_DWithin')
+
+
+def test_ST_Equals():
+    _test_simple_func('ST_Equals')
+
+
+def test_ST_Intersects():
+    _test_simple_func('ST_Intersects')
+
+
+def test_ST_Length():
+    _test_simple_func('ST_Length')
+
+
+def test_ST_OrderingEquals():
+    _test_simple_func('ST_OrderingEquals')
+
+
+def test_ST_Overlaps():
+    _test_simple_func('ST_Overlaps')
+
+
+def test_ST_Perimeter():
+    _test_simple_func('ST_Perimeter')
+
+
+def test_ST_Relate():
+    _test_simple_func('ST_Relate')
+
+
+def test_ST_Touches():
+    _test_simple_func('ST_Touches')
+
+
+def test_ST_Within():
+    _test_simple_func('ST_Within')
+
+
+def test_ST_Buffer():
+    _test_geometry_returning_func('ST_Buffer')
+
+
+def test_ST_Difference():
+    _test_geometry_returning_func('ST_Difference')
+
+
+def test_ST_Intersection():
+    _test_geometry_returning_func('ST_Intersection')
+
+
+def test_ST_Union():
+    _test_geometry_returning_func('ST_Union')
