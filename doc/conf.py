368dab9f3c3971c21d02b37a0612b1c3643733df
Éric Lemoine <eric.lemoine@gmail.com>
2012-09-07 14:33:33 +0200

    add mock to conf.py for rtd

--- doc/conf.py
+++ doc/conf.py
@@ -214,3 +214,26 @@ man_pages = [
     ('index', 'geoalchemy2', u'GeoAlchemy2 Documentation',
      [u'Eric Lemoine'], 1)
 ]
+
+# Mocks for Read the Dics
+import sys
+
+class Mock(object):
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        return Mock()
+
+    @classmethod
+    def __getattr__(self, name):
+        if name in ('__file__', '__path__'):
+            return '/dev/null'
+        elif name[0] == name[0].upper():
+            return type(name, (), {})
+        else:
+            return Mock()
+
+MOCK_MODULES = ['shapely', 'shapely.wkt', 'shapely.wkb']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = Mock()
