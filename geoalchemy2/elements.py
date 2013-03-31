c7fb2a4e4aeaa04aed4e3b76ed6972a2352b0919
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2013-03-31 19:25:58 +0200

    elements: Allow calling ST_ functions on WKTElements

--- geoalchemy2/elements.py
+++ geoalchemy2/elements.py
@@ -31,6 +31,21 @@ class _SpatialElement(object):
         return "<%s at 0x%x; %r>" % \
             (self.__class__.__name__, id(self), self.desc)  # pragma: no cover
 
+    def __getattr__(self, name):
+        #
+        # This is how things like lake.geom.ST_Buffer(2) creates
+        # SQL expressions of this form:
+        #
+        # ST_Buffer(ST_GeomFromWKB(:ST_GeomFromWKB_1), :param_1)
+        #
+
+        # We create our own _FunctionGenerator here, and use it in place of
+        # SQLAlchemy's "func" object. This is to be able to "bind" the
+        # function to the SQL expression. See also GenericFunction above.
+
+        func_ = expression._FunctionGenerator(expr=self)
+        return getattr(func_, name)
+
 
 class WKTElement(_SpatialElement, expression.Function):
     """
@@ -78,18 +93,3 @@ class WKBElement(_SpatialElement, expression.Function):
         This element's description string.
         """
         return binascii.hexlify(self.data)
-
-    def __getattr__(self, name):
-        #
-        # This is how things like lake.geom.ST_Buffer(2) creates
-        # SQL expressions of this form:
-        #
-        # ST_Buffer(ST_GeomFromWKB(:ST_GeomFromWKB_1), :param_1)
-        #
-
-        # We create our own _FunctionGenerator here, and use it in place of
-        # SQLAlchemy's "func" object. This is to be able to "bind" the
-        # function to the SQL expression. See also GenericFunction above.
-
-        func_ = expression._FunctionGenerator(expr=self)
-        return getattr(func_, name)
