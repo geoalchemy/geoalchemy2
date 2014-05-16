9be3b5bbae39b306c8f9b9db1ce3af449c7291fd
Jimmy Yuen Ho Wong <wyuenho@gmail.com>
2014-05-17 02:54:53 +0800

    Fix TypeError for PyPy
    A module object is not subscriptable in PyPy. A subscriptable module is an implementation detail for CPython. The help doc only says you can use dotted notation to get attributes out of a module, it doesn't implement __getitem__. One should also not touch __builtins__ as it is an implementation detail for CPython.

--- geoalchemy2/compat.py
+++ geoalchemy2/compat.py
@@ -4,14 +4,14 @@ Python 2 and 3 compatibility:
     - Py3k `memoryview()` made an alias for Py2k `buffer()`
     - Py3k `bytes()` made an alias for Py2k `str()`
 """
-
+import __builtin__
 import sys
 
 if sys.version_info[0] == 2:
-    buffer = __builtins__['buffer']
+    buffer = getattr(__builtin__, 'buffer')
     bytes = str
 
 else:
     # Python 2.6 flake8 workaround
-    buffer = __builtins__['memoryview']
+    buffer = getattr(__builtin__, 'memoryview')
     bytes = bytes
