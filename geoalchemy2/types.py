a7505ccf505e9e8259bad7282e61217895f2948b
Éric Lemoine <eric.lemoine@oslandia.com>
2019-04-26 10:07:31 +0200

    Avoid ewkb to ewkt conversion when using PostGIS

--- geoalchemy2/types.py
+++ geoalchemy2/types.py
@@ -155,10 +155,20 @@ class _GISType(UserDefinedType):
                 else:
                     return 'SRID=%d;%s' % (bindvalue.srid, bindvalue.data)
             elif isinstance(bindvalue, WKBElement):
-                if not SHAPELY:
-                    raise ArgumentError('Shapely required for handling WKBElement bind values')
-                shape = to_shape(bindvalue)
-                return 'SRID=%d;%s' % (bindvalue.srid, shape.wkt)
+                if dialect.name == 'sqlite' or not bindvalue.extended:
+                    # With SpatiaLite or when the WKBElement includes a WKB value rather
+                    # than a EWKB value we use Shapely to convert the WKBElement to an
+                    # EWKT string
+                    if not SHAPELY:
+                        raise ArgumentError('Shapely is required for handling WKBElement bind '
+                                            'values when using SpatiaLite or when the bind value '
+                                            'is a WKB rather than an EWKB')
+                    shape = to_shape(bindvalue)
+                    return 'SRID=%d;%s' % (bindvalue.srid, shape.wkt)
+                else:
+                    # PostGIS ST_GeomFromEWKT works with EWKT strings as well
+                    # as EWKB hex strings
+                    return bindvalue.desc
             else:
                 return bindvalue
         return process
