38eff86624a334c50bd4c2d3fd221a86ddb1815f
Adrien Berchet <adrien.berchet@gmail.com>
2022-03-02 19:31:06 +0100

    Rename pypy3 into pypy-3 in GH Actions and add py39 in classifiers

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Intended Audience :: Information Technology",
         "License :: OSI Approved :: MIT License",
         "Topic :: Scientific/Engineering :: GIS",
