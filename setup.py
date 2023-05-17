259be14b70cce2f9063734e647db2f601d3d3b53
Adrien Berchet <adrien.berchet@gmail.com>
2023-05-18 00:17:23 +0200

    Add Py311 support (#446)

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Intended Audience :: Information Technology",
         "License :: OSI Approved :: MIT License",
         "Topic :: Scientific/Engineering :: GIS",
