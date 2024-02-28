db69ffa5c5d8ad18d0700155ee9d8aa8616da04a
Travis Brown <teb@jetcom.org>
2024-02-28 07:30:47 -0500

    Add mariadb support
    MariaDB and MySQL use the same dialect, so we just make a copy of this

--- geoalchemy2/admin/__init__.py
+++ geoalchemy2/admin/__init__.py
@@ -19,6 +19,7 @@ def select_dialect(dialect_name):
     known_dialects = {
         "geopackage": dialects.geopackage,
         "mysql": dialects.mysql,
+        "mariadb": dialects.mysql,
         "postgresql": dialects.postgresql,
         "sqlite": dialects.sqlite,
     }
